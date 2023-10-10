b731b16b4e322197af758fa380179998426f7aeb
dungvannguyen <NguyDu06@risk.regn.net>
2023-10-10 21:44:50 +0700

    update sleep

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,35 @@
 // pages/index.js or any other page
 
 import Navbar from '../components/Navbar';
+import { useState, useEffect } from 'react';
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 
 function HomePage() {
+    const [contentLoaded, setContentLoaded] = useState(false);
+    const [fade, setFade] = useState(false);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setContentLoaded(true);
+        }, 3000); // 3 seconds delay or as per your preference
+    }, []);
+
+
     return (
         <div className="d-flex flex-column min-vh-100">
             <div className="flex-grow-1">
                 <Navbar />
-                <h1>from data prime</h1>
+                {contentLoaded ? (
+                    <div className={`App ${fade ? 'fade-out' : ''}`}>
+                        <h1>from data data prime</h1>
+                    </div>
+                ) : (
+                    <div>Mimic processing data of data prime...</div>
+                )}
             </div>
 
             <footer className="footer mt-auto py-3 bg-dark">
